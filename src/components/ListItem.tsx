25395a45009fc8cea2cad7860ce7cc863deadf5c
Rodrigo Fialho <rodrigo.pereira1@hotmail.com>
2022-08-02 18:19:37 -0300

    Desafio01

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -4,21 +4,33 @@ import { Check, Trash } from 'phosphor-react'
 interface ListemProps {
   id: string;
   title: string;
+  isCompleted: boolean;
   onDeleteItemList: (item: string) => void;
+  onTogleIsCompleted: (id: string) => void
 }
 
 
-export function ListItem({title, onDeleteItemList, id}: ListemProps) {
+export function ListItem({title, isCompleted, onDeleteItemList, onTogleIsCompleted, id}: ListemProps) {
 
   function handleDeleteComment() {
     onDeleteItemList(id)
   }
-  return (
 
+  function  handleTogleIsCompleted() {
+    onTogleIsCompleted(id)
+  }
+
+  return (
     <div className={styles.content}>
       <li className={styles.list}>
-        <input type="checkbox"/>
-        <p>{title}</p>
+        <input 
+          className={styles.checkboxAround} 
+          type="checkbox"  
+          checked={isCompleted}
+          onClick={handleTogleIsCompleted}
+        />
+        
+        <p className={isCompleted ? styles.taskTitleCheck : styles.taskTitleNoCheck}>{title}</p>
 
         <button 
           onClick={handleDeleteComment}
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -2,48 +2,63 @@ import styles from './Post.module.css'
 import { PlusCircle } from 'phosphor-react'
 import { ListItem } from './ListItem'
 import { FormEvent, useState } from 'react'
-import {v4 as uuidv4} from 'uuid'
+import { v4 as uuidv4 } from 'uuid'
 
 import Clipboard from '../assets/Clipboard.svg'
 
 interface ListemPostProps {
     id: string;
     title: string;
-}   
+    isCompleted: boolean;
+}
 
 export function Post() {
     const [listItem, setListItem] = useState<ListemPostProps[]>([])
     const [newItem, setNewItem] = useState('')
-   
+    const [tasksCompleteds, setTasksCompleteds] = useState(0);
+    const [tasksTotal, setTasksTotal] = useState(0);
+
     function handlenewitem(event: FormEvent) {
         event.preventDefault()
 
         const newTask = {
             id: uuidv4(),
-            title: newItem
+            title: newItem,
+            isCompleted: false
         }
 
         setListItem([...listItem, newTask])
         setNewItem('')
-
-        console.log(newTask)
+        setTasksTotal(tasksTotal + 1)
+        setTasksCompleteds(tasksCompleteds)
     }
 
     function deleteItemList(onDeleteItemList: string) {
         const deleteItem = listItem.filter(item => {
             return item.id !== onDeleteItemList
         })
-
+        setTasksTotal(tasksTotal - 1)
+        setTasksCompleteds(tasksCompleteds - 1 )
         setListItem(deleteItem)
+        
+    }
+
+    function handleTogleIsCompleted(id: string) {
+        const taskToToggle = listItem.find(task => task.id === id)
+        if(!taskToToggle) return
+        taskToToggle.isCompleted = !taskToToggle.isCompleted
+        setListItem([...listItem])
+        setTasksCompleteds(listItem.filter(task => task.isCompleted).length)
+
     }
 
     return (
         <>
             <div className={styles.main}>
                 <form onSubmit={handlenewitem} className={styles.content}>
-                    <input 
-                        className={styles.input} 
-                        type="text" 
+                    <input
+                        className={styles.input}
+                        type="text"
                         placeholder='Adicione uma nova tarefa'
                         onChange={event => setNewItem(event.target.value)}
                         value={newItem}
@@ -54,54 +69,46 @@ export function Post() {
                         <PlusCircle className={styles.icon} size={25} />
                     </button>
                 </form>
-                
-            
-    
-            <article className={styles.article}>
 
-                <div className={styles.taferas}>
-                    <>
-                    <div className={styles.counterItem}>    
-                        <p className={styles.criadas}>Tarefas criadas <strong>0</strong></p>
-                        <p className={styles.concluidas}>Concluídas <strong>0</strong></p>
+                <article className={styles.article}>
+
+                    <div className={styles.taferas}>
+                        <>
+                            <div className={styles.counterItem}>
+                                <p className={styles.criadas}>Tarefas criadas<strong>{tasksTotal}</strong></p>
+                                <p className={styles.concluidas}>Concluídas <strong>{tasksCompleteds} de {tasksTotal}</strong></p>
+                            </div>
+
+                            {listItem.length === 0
+
+                                ?
+
+                                <div className={styles.info}>
+                                    <img src={Clipboard} alt="Clipboard" />
+                                    <p>Você ainda não tem tarefas cadastradas</p>
+                                    <p>Crie tarefas e organize seus itens a fazer</p>
+                                </div>
+
+                                :
+
+                                listItem.map(item => {
+                                    return (
+                                        <ListItem
+                                            key={item.id}
+                                            id={item.id}
+                                            title={item.title}
+                                            onDeleteItemList={deleteItemList}
+                                            onTogleIsCompleted={handleTogleIsCompleted}
+                                            isCompleted={item.isCompleted}
+                                        />
+                                    )
+                                })
+
+                            }
+                        </>
                     </div>
-
-                        {/* {listItem.length === 0 
-                        
-                        ? 
-                        
-                        <div className={styles.info}>
-                            <img src={Clipboard} alt="Clipboard" />
-                            <p>Você ainda não tem tarefas cadastradas</p>
-                            <p>Crie tarefas e organize seus itens a fazer</p>
-                        </div>
-                        
-                        : 
-                        
-                        {listItem.map(item =>  {
-                            return (
-                                <ListItem 
-                                key={item.id}
-                                id={item.id}
-                                title={item.title}
-                                onDeleteItemList={deleteItemList}
-                                />
-                            )
-                        })} 
-                        
-                        } */}
-                    
-                        
-                       
-
-                        
-                    
-                    
-
-                    </>
-                </div>
-            </article>
-            </div>    
+                </article>
+            </div>
         </>
 
     )
